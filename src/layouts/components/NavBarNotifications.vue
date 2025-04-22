894e4cccf43c0e280a4cd7ce79b79f31f5040c99
Oliver Amezquita Morales <varilla24@gmail.com>
2025-04-22 13:21:51 -0600

    Removed comment notification list

--- src/layouts/components/NavBarNotifications.vue
+++ src/layouts/components/NavBarNotifications.vue
@@ -10,50 +10,6 @@ const handleNewNotification = data => {
 
 const { messages } = useWebSocket(handleNewNotification)
 
-// const notifications = ref([
-//   {
-//     id: 1,
-//     img: avatar4,
-//     title: 'Congratulation Flora! 🎉',
-//     subtitle: 'Won the monthly best seller badge',
-//     time: 'Today',
-//     is_seen: true,
-//   },
-//   {
-//     id: 2,
-//     text: 'Tom Holland',
-//     title: 'New user registered.',
-//     subtitle: '5 hours ago',
-//     time: 'Yesterday',
-//     is_seen: false,
-//   },
-//   {
-//     id: 3,
-//     img: avatar5,
-//     title: 'New message received 👋🏻',
-//     subtitle: 'You have 10 unread messages',
-//     time: '11 Aug',
-//     is_seen: true,
-//   },
-//   {
-//     id: 4,
-//     img: paypal,
-//     title: 'PayPal',
-//     subtitle: 'Received Payment',
-//     time: '25 May',
-//     is_seen: false,
-//     color: 'error',
-//   },
-//   {
-//     id: 5,
-//     img: avatar3,
-//     title: 'Received Order 📦',
-//     subtitle: 'New order received from john',
-//     time: '19 Mar',
-//     is_seen: true,
-//   },
-// ])
-
 const removeNotification = notificationId => {
   notifications.value.forEach((item, index) => {
     if (notificationId === item.id)
