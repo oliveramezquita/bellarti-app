9372bab34eada6c97f1d7a095d8a8aea11fba604
Oliver Amezquita Morales <varilla24@gmail.com>
2025-09-12 13:47:11 -0600

    Hotfix company permission

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -206,7 +206,7 @@ export default [
         title: 'Compañías',
         to: 'apps-companies',
         action: 'read',
-        subject: 'AdmCompanias',
+        subject: 'Companias',
       },
     ],
   },
